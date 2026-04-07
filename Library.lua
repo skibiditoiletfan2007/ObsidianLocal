1a39aa1d88f5c4eb943f7e25b61d27dfce1a2e0b
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2026-04-07 21:03:36 +0200

    feat: Loading GUI - AutoResizeHeight

--- Library.lua
+++ Library.lua
@@ -354,6 +354,7 @@ local Templates = {
         TotalSteps = 10,
 
         ShowSidebar = false,
+        AutoResizeHeight = false,
 
         WindowWidth = 450,
         WindowHeight = 275,
@@ -8641,11 +8642,13 @@ function Library:CreateLoading(LoadingInfo)
         TotalSteps = LoadingInfo.TotalSteps,
 
         ShowSidebar = LoadingInfo.ShowSidebar,
+        AutoResizeHeight = LoadingInfo.AutoResizeHeight,
         IsError = false,
         Destroyed = false,
 
         WindowWidth = LoadingInfo.WindowWidth,
         WindowHeight = LoadingInfo.WindowHeight,
+        BaseWindowHeight = LoadingInfo.WindowHeight,
         WindowErrorHeight = LoadingInfo.WindowHeight,
 
         ContentWidth = LoadingInfo.ContentWidth,
@@ -8833,20 +8836,22 @@ function Library:CreateLoading(LoadingInfo)
 
     local MessageLabel = New("TextLabel", {
         BackgroundTransparency = 1,
-        AutomaticSize = Enum.AutomaticSize.XY,
-        Size = UDim2.fromOffset(0, 0),
+        AutomaticSize = Loading.AutoResizeHeight and Enum.AutomaticSize.Y or Enum.AutomaticSize.XY,
+        Size = Loading.AutoResizeHeight and UDim2.new(1, -60, 0, 0) or UDim2.fromOffset(0, 0),
         Text = "",
         TextSize = 18,
+        TextWrapped = Loading.AutoResizeHeight,
         Parent = InnerContent,
     })
 
     local DescriptionLabel = New("TextLabel", {
         BackgroundTransparency = 1,
-        AutomaticSize = Enum.AutomaticSize.XY,
-        Size = UDim2.fromOffset(0, 0),
+        AutomaticSize = Loading.AutoResizeHeight and Enum.AutomaticSize.Y or Enum.AutomaticSize.XY,
+        Size = Loading.AutoResizeHeight and UDim2.new(1, -60, 0, 0) or UDim2.fromOffset(0, 0),
         Text = "",
         TextSize = 14,
         TextTransparency = 0.5,
+        TextWrapped = Loading.AutoResizeHeight,
         Parent = InnerContent,
     })
 
@@ -9029,12 +9034,35 @@ function Library:CreateLoading(LoadingInfo)
     end
 
     --// Content Page \\--
+    function Loading:RecalculateLoadingHeight()
+        if not Loading.AutoResizeHeight then
+            return
+        end
+
+        local RequiredHeight = 
+              49 -- TopBar
+            + 48 -- Padding (estimated based on layout and elements)
+            + InnerContent.UIListLayout.AbsoluteContentSize.Y
+
+        Loading.WindowHeight = math.max(Loading.BaseWindowHeight, RequiredHeight)
+    end
+
     function Loading:SetMessage(Text)
         MessageLabel.Text = Text
+
+        if Loading.AutoResizeHeight then
+            Loading:RecalculateLoadingHeight()
+            Loading:UpdateLayout()
+        end
     end
 
     function Loading:SetDescription(Text)
         DescriptionLabel.Text = Text
+
+        if Loading.AutoResizeHeight then
+            Loading:RecalculateLoadingHeight()
+            Loading:UpdateLayout()
+        end
     end
 
     function Loading:SetLoadingIcon(Icon)
